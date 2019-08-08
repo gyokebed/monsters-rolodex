03119b875d2b881fe9de671ffa2af5b5c83f79e5
Gustavo Yokebed <g.yokebed@gmail.com>
2019-08-08 17:56:45 -0500

    Updates

---
+++ precache-manifest.41e07bdbec27ee6e46f955f5b0eae6e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39dec6fd61c583d3383a6244ea90f60",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "496e869635ccc6c5ce14",
+    "url": "/monsters-rolodex/static/css/main.9392ecdc.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "496e869635ccc6c5ce14",
+    "url": "/monsters-rolodex/static/js/main.08e64ff1.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
